65c88a683948f80f9baec90f50864fe514803f8e
denyosaur <danielkim1989@gmail.com>
2023-03-16 15:26:40 -0700

    test

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -5,7 +5,8 @@ import mainReducer from './Main/reducer';
 import rootSaga from './Main/saga';
 
 const configureStore = () => {
-  const composeEnhancers = process.env.NODE_ENV !== 'production' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+  // const composeEnhancers = process.env.NODE_ENV !== 'production' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
   const reducer = combineReducers({
     main: mainReducer,
   });
